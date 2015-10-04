46f5389fedfd36d9bbbe29cfdd77e81d09fd6378
Michal Špaček <tupinek@gmail.com>
2015-10-04 10:06:05 +0200

    Add EXAMPLE4 to doc.

--- Budapest.pm
+++ Budapest.pm
@@ -196,6 +196,31 @@ For more information about Budapest Map, click L<here|https://en.wikipedia.org/w
 
 =end html
 
+=head1 EXAMPLE4
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Encode qw(encode_utf8);
+ use Map::Tube::Budapest;
+
+ # Object.
+ my $obj = Map::Tube::Budapest->new;
+
+ # Get lines.
+ my $lines_ar = $obj->get_lines;
+
+ # Print out.
+ map { print encode_utf8($_->name)."\n"; } sort @{$lines_ar};
+
+ # Output:
+ # Linia M1
+ # Linia M2
+ # Linia M3
+ # Linia M4
+
 =head1 DEPENDENCIES
 
 L<File::Share>,
